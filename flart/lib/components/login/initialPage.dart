25ed21291d896764680bb5b402b08a6e55e7e5e1
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-12 17:14:53 +0700

    error

--- flart/lib/components/login/initialPage.dart
+++ flart/lib/components/login/initialPage.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:fluid_bottom_nav_bar/fluid_bottom_nav_bar.dart';
+import 'package:flart/components/login/initialPage/account_widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -27,46 +29,29 @@ class _LoginPageState extends State<LoginPage>
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Color.fromARGB(148, 98, 172, 218),
-      bottomNavigationBar: FluidNavBar(
-        icons: [
-          FluidNavBarIcon(
-              svgPath: "assets/Login/home.svg",
-              backgroundColor: Color.fromARGB(255, 66, 255, 151),
-              extras: {"label": "home"}),
-          FluidNavBarIcon(
-              icon: Icons.bookmark_border,
-              backgroundColor: Color.fromARGB(255, 52, 236, 73),
-              extras: {"label": "bookmark"}),
-          FluidNavBarIcon(
-              svgPath: "assets/conference.svg",
-              backgroundColor: Color.fromARGB(255, 109, 192, 255),
-              extras: {"label": "conference"}),
-        ],
-        style: FluidNavBarStyle(
-            barBackgroundColor: Color.fromARGB(255, 99, 241, 212),
-            iconBackgroundColor: Color.fromARGB(255, 99, 241, 212),
-            iconSelectedForegroundColor: Color.fromARGB(255, 35, 0, 0),
-            iconUnselectedForegroundColor:
-                const Color.fromARGB(255, 255, 255, 255)),
-        scaleFactor: 2,
-        defaultIndex: 1,
-        onChange: _handleNavigationChange,
+      home: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AccountWidget(
+                    name: 'name',
+                    image: Image.file(File('assets/icons/image.png')),
+                    size: const Size(150, 150),
+                    color: Colors.yellow),
+                AccountWidget(
+                    name: 'name',
+                    image: Image.file(File('assets/icons/image.png')),
+                    size: const Size(150, 150),
+                    color: Colors.red)
+              ],
+            )
+          ],
+        ),
       ),
-    ));
-  }
-
-  void _handleNavigationChange(int index) {
-    setState(() {
-      switch (index) {
-        case 0:
-          break;
-        case 1:
-          break;
-        case 2:
-          break;
-      }
-    });
+    );
   }
 }
