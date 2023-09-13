613ccff43ffc640880ec94b9a507a8f071851fed
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-13 16:22:18 +0700

    animation check in

--- flart/lib/components/login/initialPage.dart
+++ flart/lib/components/login/initialPage.dart
@@ -3,53 +3,57 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flart/components/login/initialPage/account_widget.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
+class InitialPage extends StatefulWidget {
+  const InitialPage({super.key, required this.visible});
+  final bool visible;
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<InitialPage> createState() => _InitialPageState(visible);
 }
 
-class _LoginPageState extends State<LoginPage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _InitialPageState extends State<InitialPage> {
+  bool _visible = true;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
+  _InitialPageState(bool visible) {
+    _visible = visible;
   }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Column(
+    return Material(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              AccountWidget(
-                  name: 'name',
-                  image: AssetImage('assets/icons/image.png'),
-                  size: Size(150, 150),
-                  color: Colors.yellow),
-              AccountWidget(
-                  name: 'name',
-                  image: AssetImage('assets/icons/image.png'),
-                  size: Size(150, 150),
-                  color: Colors.red)
+              AnimatedOpacity(
+                opacity: _visible ? 1 : 0,
+                duration: const Duration(milliseconds: 1000),
+                child: const AccountWidget(
+                    name: 'name',
+                    image: AssetImage('assets/icons/image.png'),
+                    size: Size(150, 150),
+                    color: Colors.yellow),
+              ),
+              AnimatedOpacity(
+                opacity: _visible ? 1 : 0,
+                duration: const Duration(milliseconds: 1000),
+                child: const AccountWidget(
+                    name: 'name',
+                    image: AssetImage('assets/icons/image.png'),
+                    size: Size(150, 150),
+                    color: Colors.red),
+              ),
             ],
           )
         ],
       ),
     );
+  }
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
   }
 }
--- flart/lib/screens/login.dart
+++ flart/lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flart/components/login/initialPage.dart';
+import 'package:flart/components/login/initialPage/account_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
@@ -9,40 +10,80 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class _LoginScreenState extends State<LoginScreen> {
+  bool _visible = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Builder(
-          builder: (context) => LiquidSwipe(initialPage: 1, pages: pages)),
+          builder: (context) => LiquidSwipe(
+                initialPage: 1,
+                pages: [
+                  Container(
+                    color: Colors.green,
+                  ),
+                  Container(
+                    color: Colors.red[50],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            AnimatedOpacity(
+                              opacity: _visible ? 1 : 0,
+                              duration: const Duration(milliseconds: 500),
+                              child: const AccountWidget(
+                                  name: 'name',
+                                  image: AssetImage('assets/icons/image.png'),
+                                  size: Size(150, 150),
+                                  color: Colors.yellow),
+                            ),
+                            AnimatedOpacity(
+                              opacity: _visible ? 1 : 0,
+                              duration: const Duration(milliseconds: 500),
+                              child: const AccountWidget(
+                                  name: 'name',
+                                  image: AssetImage('assets/icons/image.png'),
+                                  size: Size(150, 150),
+                                  color: Colors.red),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                  ),
+                ],
+                onPageChangeCallback: _handlePageOnChange,
+              )),
     );
   }
 
-  final pages = [
-    Container(
-      color: Colors.green,
-    ),
-    Container(
-      color: Colors.red[50],
-      child: LoginPage(),
-    ),
-    Container(
-      color: Colors.blue,
-    ),
-  ];
+  void _handlePageOnChange(int index) {
+    setState(() {
+      switch (index) {
+        case 0:
+          _visible = false;
+          break;
+        case 1:
+          _visible = true;
+          break;
+        case 2:
+          _visible = false;
+          break;
+      }
+    });
+  }
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+      print('trigger on');
+    });
+  }
 }
