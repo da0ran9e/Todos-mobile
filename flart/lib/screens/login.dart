8b65c9a40edc95d4f40944ad4ab4433601afa831
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-14 10:43:15 +0700

    reoder

--- flart/lib/screens/login.dart
+++ flart/lib/screens/login.dart
@@ -18,35 +18,28 @@ class _LoginScreenState extends State<LoginScreen> {
     return MaterialApp(
       home: Builder(
           builder: (context) => LiquidSwipe(
-                initialPage: 1,
+                initialPage: 0,
                 pages: [
-                  Container(
-                    color: Colors.green,
-                  ),
                   Container(
                     color: Colors.red[50],
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
                       children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             SizedBox(
                               width: 200,
-                              height: 350,
+                              height: 200,
                               child: Stack(
                                 children: <Widget>[
                                   AnimatedPositioned(
-                                    top: _visible ? 50.0 : 150.0,
+                                    top: _visible ? 0.0 : 150.0,
                                     duration: const Duration(seconds: 2),
                                     curve: Curves.fastOutSlowIn,
                                     child: GestureDetector(
-                                      onTap: () {
-                                        setState(() {
-                                          _visible = !_visible;
-                                        });
-                                      },
                                       child: AccountWidget(
                                           name: 'name',
                                           image: const AssetImage(
@@ -65,11 +58,72 @@ class _LoginScreenState extends State<LoginScreen> {
                             ),
                             SizedBox(
                               width: 200,
-                              height: 350,
+                              height: 200,
                               child: Stack(
                                 children: <Widget>[
                                   AnimatedPositioned(
-                                    top: _visible ? 50.0 : 150.0,
+                                    top: _visible ? 0.0 : 150.0,
+                                    duration: const Duration(seconds: 2),
+                                    curve: Curves.fastOutSlowIn,
+                                    child: AnimatedOpacity(
+                                      opacity: _visible ? 1 : 0,
+                                      duration:
+                                          const Duration(milliseconds: 2000),
+                                      child: AccountWidget(
+                                        name: 'name',
+                                        image: const AssetImage(
+                                            'assets/icons/image.png'),
+                                        size: const Size(150, 150),
+                                        color: Colors.red,
+                                        onPressed: () {
+                                          setState(() {
+                                            _visible = !_visible;
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 200,
+                              height: 200,
+                              child: Stack(
+                                children: <Widget>[
+                                  AnimatedPositioned(
+                                    top: _visible ? 0.0 : 150.0,
+                                    duration: const Duration(seconds: 2),
+                                    curve: Curves.fastOutSlowIn,
+                                    child: GestureDetector(
+                                      child: AccountWidget(
+                                          name: 'name',
+                                          image: const AssetImage(
+                                              'assets/icons/image.png'),
+                                          size: Size(150, 150),
+                                          color: Colors.yellow,
+                                          onPressed: () {
+                                            setState(() {
+                                              _visible = !_visible;
+                                            });
+                                          }),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: 200,
+                              height: 200,
+                              child: Stack(
+                                children: <Widget>[
+                                  AnimatedPositioned(
+                                    top: _visible ? 0.0 : 150.0,
                                     duration: const Duration(seconds: 2),
                                     curve: Curves.fastOutSlowIn,
                                     child: AnimatedOpacity(
@@ -97,6 +151,9 @@ class _LoginScreenState extends State<LoginScreen> {
                         )
                       ],
                     ),
+                  ),
+                  Container(
+                    color: Colors.green,
                   ),
                   Container(
                     color: Colors.blue,
